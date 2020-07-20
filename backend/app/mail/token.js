2928a1e7b0593ed57c6194cf72e4755c575684f8
Carole Barbier <carole83460@gmail.com>
2020-07-20 17:10:37 +0200

    update pb import back

--- backend/app/mail/token.js
+++ backend/app/mail/token.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const EMAIL_SECRET = process.env.TOKENKEY; 
 
 const payload = {
-    pseudo: 'Nico',
-    email: 'nicolas.garcon@gmail.com',
-    password: 'motdepasse'
+    pseudo: 'Carole',
+    email: 'moovybox.cb83@gmail.com',
+    password: 'ProjetApo83*'
 }; 
 
 const token = jwt.sign(payload, EMAIL_SECRET,{expiresIn: '1d'}); 
